61a23030e1eed2f301e35f75b7bd341eb0b0b667
JR Mishra <jrmnitdgp@gmail.com>
2020-09-28 14:59:37 +0530

    [JR Mishra] Add. Player rolled the dice and got score

--- Program.cs
+++ Program.cs
@@ -12,7 +12,18 @@ namespace SnakeLadderGame
             int playerScore;
             playerScore = 0;
             Console.WriteLine("Initial Player Score : " + playerScore);
+
+            playerScore = RollTheDie();
+            Console.WriteLine("After rolling the die, score is " + playerScore);
+
             return;
+        }
+
+        static int RollTheDie()
+        {
+            Random random = new Random();
+            int score = random.Next(1, 7);
+            return score;
         }
     }
 }
